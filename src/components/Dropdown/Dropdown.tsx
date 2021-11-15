559b2427746df79de595ab765f75ae73c282472c
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-11-15 19:56:58 +0300

    Written docs to Dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -2,8 +2,19 @@ import React, { FC, useState } from "react";
 import "./Dropdown.scss";
 
 export type DropdownPropsType = {
+  /**
+   * Дочерние элементы Dropdown
+   */
   children: React.ReactNode;
+
+  /**
+   * Элемент триггер, по клику, на который открывается popup
+   */
   toggle: React.ReactNode;
+
+  /**
+   * Направление раскрытия popup
+   */
   direction?:
     | "bottom-left"
     | "bottom-center"
@@ -11,11 +22,26 @@ export type DropdownPropsType = {
     | "top-left"
     | "top-center"
     | "top-right";
+
+  /**
+   * Убрать стрелочку направления popup
+   */
   clearly: boolean;
+
+  /**
+   * Dropdown в полный размер родительского блока
+   */
   fullwidth: boolean;
+
+  /**
+   * Dropdown скрывается при клике прочие элементы
+   */
   dismissible: boolean;
 };
 
+/**
+ * Компонент для создания выпадающего списка
+ */
 const Dropdown: FC<DropdownPropsType> = ({
   children,
   toggle,
