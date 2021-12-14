623bfd7d373cde3aa80009378c285a930faa3a92
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-12-14 23:21:40 +0300

    feat: creation unit-tests to Dialog component

--- src/components/Dialog/Dialog.test.tsx
+++ src/components/Dialog/Dialog.test.tsx
@@ -17,4 +17,22 @@ describe("Dialog testing", () => {
 
     expect(dialog.find(".dialog-content").text()).toEqual("dialog-content");
   });
+
+  it("Dialog toggle blur testing", () => {
+    const dialog = shallow(
+      <Dialog size="xl" toggle={<Button>Test</Button>}>
+        dialog-content
+      </Dialog>
+    );
+
+    expect(dialog.find(".dialog-body").length).toEqual(0);
+
+    dialog.find(".dialog-toggle").simulate("click");
+
+    expect(dialog.find(".dialog-body").text()).toEqual("dialog-content");
+
+    dialog.find(".dialog-wrapper").simulate("click");
+
+    expect(dialog.find(".dialog-body").length).toEqual(0);
+  });
 });
