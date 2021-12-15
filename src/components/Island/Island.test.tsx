81a8bfd6eb64ade9b9835bad935f1cddfd6712dc
Vasily Sviridov <sviridov@sviridovcomp.ru>
2021-12-15 17:13:23 +0300

    feat: creation unit-tests to Island component

---
+++ src/components/Island/Island.test.tsx
@@ -0,0 +1,45 @@
+import { shallow } from "enzyme";
+import Island from "@components/Island/Island";
+import React from "react";
+
+describe("Island testing", () => {
+  type IslandSizeType = "xs" | "sm" | "md" | "lg" | "xl";
+  type IslandAlignType = "left" | "center" | "right";
+  type IslandTypeType = "shadow" | "border";
+  it("Island size rendering", () => {
+    const islandSizes: IslandSizeType[] = ["xs", "sm", "md", "lg", "xl"];
+    islandSizes.forEach((size) => {
+      const island = shallow(<Island size={size}>island-content</Island>);
+      expect(island.find(`.island-size_${size}`).text()).toEqual(
+        "island-content"
+      );
+    });
+  });
+
+  it("Island align rendering", () => {
+    const islandAligns: IslandAlignType[] = ["left", "center", "right"];
+    islandAligns.forEach((align) => {
+      const island = shallow(<Island align={align}>island-content</Island>);
+      expect(island.find(`.island-align_${align}`).text()).toEqual(
+        "island-content"
+      );
+    });
+  });
+
+  it("Island type rendering", () => {
+    const islandTypes: IslandTypeType[] = ["border", "shadow"];
+    islandTypes.forEach((type) => {
+      const island = shallow(<Island type={type}>island-content</Island>);
+      expect(island.find(`.island-type_${type}`).text()).toEqual(
+        "island-content"
+      );
+    });
+  });
+
+  it("Island hoverable rendering", () => {
+    const island = shallow(<Island hoverable>island-content</Island>);
+    expect(island.find(".island-hoverable").text()).toEqual(
+      "island-content"
+    );
+  });
+});
