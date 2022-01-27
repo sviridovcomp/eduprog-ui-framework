7dcba9b3ec0ed3cd04ee5cb6239f243115cf31ed
Vasily Sviridov <sviridov@sviridovcomp.ru>
2022-01-27 18:56:45 +0300

    feat: creation component switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -4,14 +4,23 @@ import classList from "@utils/classList/classList";
 
 export type SwitchProps = {
 	label?: string;
+	position?: "left" | "right";
 	color?: "primary" | "secondary" | "accent";
 }
 
-const Switch: FC<SwitchProps> = ({label , color}) => {
+const Switch: FC<SwitchProps> = ({label , color, position= "left"}) => {
 	return (
 		<label className="switch">
+			{ position == "left" && (
+				<div className="switch-additional">{label}</div>
+			)}
+
 			<input type="checkbox"/>
-			<span className={classList(["switch-slider", color ? `switch-slider-${color}` : ""])}></span>
+			<span className={classList(["switch-slider", color ? `switch-slider-${color}` : ""])}/>
+
+			{ position == "right" && (
+				<div className="switch-additional">{label}</div>
+			)}
 		</label>
 	);
 };
