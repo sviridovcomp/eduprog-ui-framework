8efe7e96eb37db9dbb8de17e5ececac3c668d3e6
Vasily Sviridov <sviridov@sviridovcomp.ru>
2022-01-27 18:58:51 +0300

    feat: creation component switch

--- dist/components/Switch/Switch.d.ts
+++ dist/components/Switch/Switch.d.ts
@@ -2,6 +2,7 @@ import { FC } from "react";
 import "./Switch.scss";
 export declare type SwitchProps = {
     label?: string;
+    position?: "left" | "right";
     color?: "primary" | "secondary" | "accent";
 };
 declare const Switch: FC<SwitchProps>;
