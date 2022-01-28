9c0260de881dcf56000f5db24c7982a09ca64c5d
Vasily Sviridov <sviridov@sviridovcomp.ru>
2022-01-29 00:56:46 +0300

    feat: updated row component

--- src/components/Grid/Row/Row.tsx
+++ src/components/Grid/Row/Row.tsx
@@ -1,15 +1,19 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import "./Row.scss";
 import classList from "@utils/classList/classList";
 
 export type RowPropsType = {
-    align?: "start" | "end" | "center";
-    spacing?: "1" | "2" | "3" | "4" | "5" | "6" | number;
+  align?: "start" | "end" | "center";
+  spacing?: "1" | "2" | "3" | "4" | "5" | "6" | number;
 };
 
-const Row: FC<RowPropsType> = ({children, spacing}) => {
-    const rootClasses = classList(["ep-row", spacing ? `ep-row_spacing-${spacing}` : ""]);
-    return <div className={rootClasses}>{children}</div>;
+const Row: FC<RowPropsType> = ({ children, spacing, align }) => {
+  const rootClasses = classList([
+    "ep-row",
+    spacing ? `ep-row_spacing-${spacing}` : "",
+    align ? `ep-row_align_${align}` : "",
+  ]);
+  return <div className={rootClasses}>{children}</div>;
 };
 
 export default Row;
