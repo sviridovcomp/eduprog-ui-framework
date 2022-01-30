a7459aa86ac905265be9854a6f5c49617c5c6583
Vasily Sviridov <sviridov@sviridovcomp.ru>
2022-01-31 00:34:43 +0300

    feat: updated switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -34,7 +34,7 @@ const Switch: FC<SwitchProps> = ({
     <label className={classList(["switch", className])} style={style}>
       {position == "left" && <div className="switch-additional">{label}</div>}
 
-      <input type="checkbox" onClick={onClick} name={name} />
+      <input type="checkbox" onClick={onClick} name={name} checked={active} />
       <span
         className={classList([
           "switch-slider",
