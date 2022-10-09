fb85e0f872a83166e71a7c5f417162b7ad3456c7
Vasily Sviridov <vasily-sviridov@eduprog-team.ru>
2022-10-09 15:40:55 +0300

    fix: update

--- dist/components/Badge/Badge.d.ts
+++ dist/components/Badge/Badge.d.ts
@@ -4,6 +4,7 @@ import "./Badge.scss";
 export declare type BadgeProps = defaultProps & {
     view: "success" | "error" | "warning";
     children?: React.ReactNode;
+    withoutDot?: boolean;
 };
 declare const Badge: FC<BadgeProps>;
 export default Badge;
